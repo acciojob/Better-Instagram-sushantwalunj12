e35d0ae2ec33f3cf9f320d9bf98d12c0e15b974e
Sushant Walunj <88195944+sushantwalunj12@users.noreply.github.com>
2023-03-01 14:48:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your code here
+let draggedItem = null;
+
+function allowDrop(event) {
+  event.preventDefault();
+}
+
+function drag(event) {
+  draggedItem = event.target;
+}
+
+function drop(event) {
+  event.preventDefault();
+  let droppedItem = event.target;
+  let tempBackground = draggedItem.style.backgroundImage;
+  draggedItem.style.backgroundImage = droppedItem.style.backgroundImage;
+  droppedItem.style.backgroundImage = tempBackground;
+}
+
+const images = document.querySelectorAll(".image");
+images.forEach((image) => {
+  image.addEventListener("dragstart", drag);
+  image.addEventListener("dragover", allowDrop);
+  image.addEventListener("drop", drop);
+});
